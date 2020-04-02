b37b7ae4cf37d67f9b479a10a071fffee09110ad
ahmed <ahmed@192.168.1.7>
2020-04-02 15:32:55 +0200

    uiStack

--- Introduction to Stacks/src/eg/edu/alexu/csd/datastructure/stack/UI_Stack.java
+++ Introduction to Stacks/src/eg/edu/alexu/csd/datastructure/stack/UI_Stack.java
@@ -12,7 +12,8 @@ public class UI_Stack {
 		String input;
 		while (true) {
 			System.out.println("Please choose an action\n---------------------------\n1)push an element to stack\n"
-					+ "2)pop an element from the stack\n3)the top element of the stack\n4)the size of the stack\n5)exit");
+					+ "2)pop an element from the stack\n3)the top element of the stack\n4)the size of the stack\n"
+					+ "5)check if the Stack is empty\n6)exit");
 			choose = scan.next().charAt(0);
 			switch(choose) {
 			case '1': System.out.println("enter your element");
@@ -32,7 +33,9 @@ public class UI_Stack {
 					break;
 			case '4':System.out.println("the size equals : "+s.size());
 					break;
-			case '5':System.exit(0); 
+			case '5':System.out.println("the Stack is empty ? : "+s.isEmpty());
+					break;
+			case '6':System.exit(0); 
 			
 			default:System.out.println("please choose an invalid integer");
 			
