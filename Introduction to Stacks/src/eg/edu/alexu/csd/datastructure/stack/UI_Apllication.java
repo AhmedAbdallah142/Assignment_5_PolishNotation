c804c6dc32a33653276e377d50b8b5df4989b8a1
ahmed <ahmed@192.168.1.7>
2020-04-03 18:12:08 +0200

    modify UIApp

--- Introduction to Stacks/src/eg/edu/alexu/csd/datastructure/stack/UI_Apllication.java
+++ Introduction to Stacks/src/eg/edu/alexu/csd/datastructure/stack/UI_Apllication.java
@@ -1,7 +1,6 @@
 package eg.edu.alexu.csd.datastructure.stack;
 
 import java.util.Scanner;
-import eg.edu.alexu.csd.datastructure.linkedList.csX22.Single_Linked_list;
 
 public class UI_Apllication {
 	public static void main (String [] args) {
@@ -11,33 +10,17 @@ public class UI_Apllication {
 		String expression;
 		System.out.println("\t\t\t\t\twelcome to infix to postfix program");
 		while(true) {
-			Single_Linked_list l = new Single_Linked_list();
 			System.out.print("\t\t\t\t\t-----------------------------------\nEnter the infix notation (digits or symbols): ");
 			try {
 				expression =e.infixToPostfix(s.next()); 
 				System.out.println("the postfix notation is : "+expression);
 				for (int i=0;i<expression.length();i++) {
 					if (e.checkSymbols(expression.charAt(i))) {
-						l.add(expression.charAt(i));
+						System.out.print("Enter the value of "+expression.charAt(i)+" : ");
+						expression=expression.replace(expression.substring(i,i+1),s.next());
 					}
 				}
-				if (l.isEmpty()) {
-					System.out.println("the value of the postfix notation is : "+e.evaluate(expression));	
-				}
-				else {
-					for (int i=0;i<l.size();i++) {
-						System.out.print("Enter the value of "+l.get(i)+" : ");
-						l.set(i, s.next());
-					}
-					int x = 0;
-					for (int i=0;i<expression.length();i++) {
-						if (e.checkSymbols(expression.charAt(i))) {
-							expression=expression.replace(expression.substring(i,i+1), (String)l.get(x));
-							x++;
-						}
-					}
-					System.out.println("the value of the postfix notation is : "+e.evaluate(expression));
-				}	
+				System.out.println("the value of the postfix notation is : "+e.evaluate(expression));
 			}
 			catch (Exception x) {
 				System.out.println(x.getMessage());
